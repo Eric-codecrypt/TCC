f87882f81eac90f2319b583e327d40d9863b4aad
T1NT4 <thiagogabriel.osil@gmail.com>
2025-10-22 15:46:38 -0300

    nem lembro oq q eu tava fazendo

--- View/UserView.php
+++ View/UserView.php
@@ -34,28 +34,54 @@ $nome_arquivo_fotoperfil = $Controller->getFotoPerfil($user['nome_arquivo_fotope
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($primeiro_nome); ?> - Move On Fitness</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/carousel.css">
+    <link rel="stylesheet" href="css/basics.css">
     <link rel="stylesheet" type="text/css" href="font-awesome/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
 </head>
 
 <body>
     <?php include __DIR__ . "/header.php"; ?>
-    <div>
         <section class="user-sect">
-            <div>
-                <form class="pfp" method="POST" action="../user-actions/process_edit_pfp.php" enctype="multipart/form-data">
-                    <?php if (isset($user)): ?>
-                        <img src="IMG/pfps/<?= $nome_arquivo_fotoperfil ?>">
-                    <?php endif; ?>
-                    <label for="foto_perfil">
-                        <img src="IMG/mudarFotoPerfil.png" alt="">
-                    </label>
-                    <input type="file" name="foto_perfil" id="foto_perfil" onchange="this.form.submit()">
+            <div class="flex-row gap10 flex-row">
+                <form class="pfp sticky" method="POST" action="../user-actions/process_edit_pfp.php" enctype="multipart/form-data">
+                        <?php if (isset($user)): ?>
+                            <img src="IMG/pfps/<?= $nome_arquivo_fotoperfil ?>">
+                        <?php endif; ?>
+                        <label for="foto_perfil">
+                            <img src="IMG/mudarFotoPerfil.png" alt="">
+                        </label>
+                        <input type="file" name="foto_perfil" id="foto_perfil" onchange="this.form.submit()">
                 </form>
+                <div class="grow-100">
+                    <div class="height-100po info-text flex-column gap10">
+                        <div>
+                            <p>Nome:</p>
+                            <h3><?=$nome_completo?></h3>
+                        </div>
+                        <div class="flex-row justify-between gap30 info-text-small">
+                            <?php if($user['CPF'] != null OR $user['CPF'] != ''):?>
+                                <div>
+                                    <p>CPF:</p>
+                                    <h3><?=$user['CPF']?></h3>
+                                </div>
+                            <?php endif;?>
+                            <div>
+                                <p>Email:</p>
+                                <h3><?=$user['email']?></h3>
+                            </div>
+                        </div>
+                        <div class="flex-row justify-between gap30 info-text-small">
+                            <?php if($user['celular'] != null OR $user['celular'] != ''):?>
+                                <div>
+                                    <p>celular:</p>
+                                    <h3><?=$user['celular']?></h3>
+                                </div>
+                            <?php endif;?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
-    </div>
     <?php include __DIR__ . "/footer.php"; ?>
 </body>
 
